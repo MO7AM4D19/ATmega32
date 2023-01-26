a2822631a6a4eafb9d498185228353e4df3648b3
MO7AM4D19 <anismohamed@std.mans.edu.eg>
2023-01-26 18:56:00 +0200

    EEPROM driver V1.00

--- 2-HAL/7-EEPROM/EEPROM_program.c
+++ 2-HAL/7-EEPROM/EEPROM_program.c
@@ -11,10 +11,22 @@
 
 void EEPROM_voidWriteByte(u8 Copy_u8DataByte,u16 Copy_u16MemoryAddress)
 {
-    
+    TWI_SendStartCondition();
+    TWI_SendSlaveAddressWithWrite(((0xA0 | (MEMORY_BUSS_ID<<3)) | (u8)(Copy_u16MemoryAddress>>8)) + 1);
+    TWI_MasterWriteDataByte((u8)Copy_u16MemoryAddress);
+    TWI_MasterWriteDataByte(Copy_u8DataByte);
+    TWI_SendStopCondition();
 }
 
 u8 EEPROM_u8ReadByte(u16 Copy_u16MemoryAddress)
 {
-
+    u8 Local_u8Data;
+    TWI_SendStartCondition();
+    TWI_SendSlaveAddressWithWrite(((0xA0 | (MEMORY_BUSS_ID<<3)) | (u8)(Copy_u16MemoryAddress>>8)) + 1);
+    TWI_MasterWriteDataByte((u8)Copy_u16MemoryAddress);
+    TWI_SendRepeatedStart();
+    TWI_SendSlaveAddressWithRead(((0xA0 | (MEMORY_BUSS_ID<<3)) | (u8)(Copy_u16MemoryAddress>>8)) + 1);
+    TWI_MasterReadDataByte(&Local_u8Data);
+    TWI_SendStopCondition();
+    return Local_u8Data;
 }
